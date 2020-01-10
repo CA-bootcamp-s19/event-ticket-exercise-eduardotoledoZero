fa75b3f58a13155055e9944f1a5366d6318ba241
Eduardo Toledo <eduardo.toledo@imappginate.com>
2020-01-10 14:29:34 -0800

    TestEventsTickets.sol V1.0

--- test/TestEventTicket.sol
+++ test/TestEventTicket.sol
@@ -7,6 +7,30 @@ import "../contracts/EventTicketsV2.sol";
 
 contract TestEventTicket {
 
+EventTickets  eventTickets;
+TicketsBuyer buyer;
+    
+constructor() public payable{}
+    
+ function beforeEach() public {
+    eventTickets = EventTickets(DeployedAddresses.EventTickets());  
+    buyer = new TicketsBuyer();
+    address(buyer).transfer(100);
+    Assert.equal(address(buyer).balance, 100, "Buyer initial balance should be 100 wei.");
 
+ }
+
+
+}
+
+contract TicketsBuyer{
+    constructor() public payable{}
+    function buyTicket(EventTickets _eventTickets, uint paidAmount,uint _tickets) public returns(bool){
+        //address(_eventTickets).call.value(paidAmount)(abi.encodeWithSignature("buyTickets(uint256)", tickets);
+        _eventTickets.buyTickets.value(paidAmount)(_tickets);
+    }
+
+
+    function  () external payable{}
 
 }
